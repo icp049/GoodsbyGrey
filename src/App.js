2b1f675e364b1e9dc3b229b344533799ba485d9d
icp049 <ianpepe2000@gmail.com>
2023-06-11 00:26:06 -0600

    finished all the pages and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom'
+
+
+//import the pages
+
+import Home from './pages/Home';
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
+import Search from "./pages/Search";
+
+
+//import the components
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+
+//do the layout
+
+const Layout = () => {
+  return(
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer />
     </div>
   );
-}
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children: [
+      {path : '/', element: <Home/>},
+      {path : '/products/:id', element: <Products/>},
+      {path : '/product/:id', element: <ProductDetails/>},
+      {path : '/search', element: <Search/>},
+    ],
+  },
+]
+)
+
+
+
+
+const App = () => {
+  return <div> 
+        <RouterProvider router = {router} />
+
+
+
+
+  </div>
+};
 
 export default App;
